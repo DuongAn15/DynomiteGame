4da207444e95945a245d393d49e4de4ee598d2c7
NAMHAIIT2HUST <namhai23092005@gmail.com>
2026-07-12 07:32:10 +0700

    Step 2: Inject BGM into UI views

--- TouchGFX/gui/src/homescreen_screen/HomeScreenView.cpp
+++ TouchGFX/gui/src/homescreen_screen/HomeScreenView.cpp
@@ -1,5 +1,7 @@
 #include <gui/homescreen_screen/HomeScreenView.hpp>
 #include "stm32f4xx_hal.h"
+#include <gui/common/AudioManager.hpp>
+#include "audio_bgm_menu.h"
 
 HomeScreenView::HomeScreenView()
 {
@@ -9,6 +11,7 @@ HomeScreenView::HomeScreenView()
 void HomeScreenView::setupScreen()
 {
     HomeScreenViewBase::setupScreen();
+    AudioManager::playBGM(audio_bgm_menu, audio_bgm_menu_length);
 }
 
 void HomeScreenView::tearDownScreen()
